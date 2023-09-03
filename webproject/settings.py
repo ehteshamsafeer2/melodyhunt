4f3021302175fdb0c1308225ba3cea07ec872b20
ehteshamsafeer2 <ehtesham2020@namal.edu.pk>
2023-09-03 05:14:02 -0700

    master branch changed to main

--- webproject/settings.py
+++ webproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zq$kvo)!=63mjl)td2gbc0=xly5(30423ltqh8vc&q^%d*b=+e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
